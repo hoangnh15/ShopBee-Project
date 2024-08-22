638e6b09626bd89f75db3156753807782ceac52f
Hoang Nguyen <21522094@gm.uit.edu.vn>
2024-08-22 18:45:56 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,'/view/index.html'));
+} );
+// use boostrap
+app.use('/images', express.static(path.join(__dirname, 'view/images')));
+app.use('/css', express.static(path.join(__dirname, 'view/css')));
+app.use('/js', express.static(path.join(__dirname, 'view/js')));
+app.use('/assets', express.static(path.join(__dirname, 'view/assets')));
+
+app.listen(3000, () => {
+    console.log('Server is running at PORT 3000');
+});
