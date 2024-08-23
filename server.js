15f1b29e28b4121a4a2f0a9d2304b61bafcd2d97
Hoang Nguyen <21522094@gm.uit.edu.vn>
2024-08-23 13:11:04 +0700

    Responesive header and product_tab_content

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/images', express.static(path.join(__dirname, 'view/images')));
 app.use('/css', express.static(path.join(__dirname, 'view/css')));
 app.use('/js', express.static(path.join(__dirname, 'view/js')));
 app.use('/assets', express.static(path.join(__dirname, 'view/assets')));
-
+app.use('/custom', express.static(path.join(__dirname, 'view/custom')));
 app.listen(3000, () => {
     console.log('Server is running at PORT 3000');
 });
