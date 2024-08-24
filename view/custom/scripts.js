d96964b4855749d5d68226741b9714247bc78a92
Tú Bùi Đức Anh <21522735@ms.uit.edu.vn>
2024-08-24 11:52:58 +0700

    Fix card

--- view/custom/scripts.js
+++ view/custom/scripts.js
@@ -6,28 +6,40 @@ document.addEventListener('DOMContentLoaded', function() {
     var tab_content_left = document.getElementById('tab_product_content_left');
     var tab_content_right = document.getElementById('tab_product_content_right');
     var product_banner_left = document.getElementById('product_banner_left');
+    function hideAll() {
+        phoneContent.style.display = 'none';
+        laptopContent.style.display = 'none';
+        tabletContent.style.display = 'none';
+    }
     button1.addEventListener('click', function(){
+        hideAll();
+        phoneContent.style.display = 'block';
         button1.style.backgroundColor = '#ed4e2e';
         button2.style.backgroundColor = '#eaedf1';
         button3.style.backgroundColor = '#eaedf1';
         tab_content_left.style.backgroundColor = tab_content_right.style.backgroundColor = '#ed4e2e';
         product_banner_left.src = 'images/banner/banner_dt.png';
+        tab_content_right.innerHTML = phonecontent;
     });
     button2.addEventListener('click', function(){
+        hideAll();
+        laptopContent.style.display = 'block';
         button2.style.backgroundColor = '#ffd744';
         button1.style.backgroundColor = '#eaedf1';
         button3.style.backgroundColor = '#eaedf1';
         tab_content_left.style.backgroundColor = tab_content_right.style.backgroundColor = '#ffd744';
         product_banner_left.src = 'images/banner/backtoschool.png';
-        
-        
+        tab_content_right.innerHTML = laptopcontent;
     });
     button3.addEventListener('click', function(){
+        hideAll();
+        tabletContent.style.display = 'block';
         button3.style.backgroundColor = '#b3e3f0';
         button2.style.backgroundColor = '#eaedf1';
         button1.style.backgroundColor = '#eaedf1';
         tab_content_left.style.backgroundColor = tab_content_right.style.backgroundColor = '#b3e3f0';
         product_banner_left.src = 'images/banner/banner_3_tab.webp';
+        tab_content_right.innerHTML = phonecontent;
     });
 
 });
